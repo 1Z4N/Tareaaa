c0e37652f4eae1903d72d85fe4c879da5da01fa0
1Z4N <nini02746@gmail.com>
2021-07-22 21:34:08 +0200

    Tareaa1

---
+++ Tarea1.py
@@ -0,0 +1,68 @@
+#Pedir la edad a un usuario y decir si puede ir a votar o no
+edad = input("Edad:")
+
+edad = int(edad)
+
+if edad < 18:
+    print('No tienes la edad para votar')
+
+else:
+    print("Eres apto para votar")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#yoquese = input("Escribe algo:")
+#yoquese = int(yoquese)
+#print(yoquese)
+
+#vida = 100
+#vida = vida - yoquese
+#damageArma = 10
+#ultiBrutalDeKarthus = 100
+#pocion = 50
+#vida = vida - damageArma
+#vida = vida - damageArma
+#vida = vida - ultiBrutalDeKarthus
+
+
+#if vida <= 0:
+    #print("El jugador ha muerto")
+    #print("FIN DEL JUEGO")
+#else:
+    #print("Pues no ha palmao")
+
+#print(vida)
